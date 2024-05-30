a865e4ce71ee090830b56e5c322e43a5e86b90b8
dibya.borah <dibyajyotiborah469@gmail.com>
2024-05-30 11:16:43 +0530

    learned watch function of RHF

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,35 @@
+import { useForm } from "react-hook-form";
+
+let renderCount = 0;
 const App = () => {
+  console.log('component renders')
+  const { register, formState: {errors}, watch } = useForm({
+    mode: 'all',
+    defaultValues: {
+      firstName: 'bill',
+      lastName: 'luo'
+    }
+  });
+  // console.log('watch', watch());// watch function trigger re-render of the component
+  const firstName = watch('firstName');
+  console.log('watching just firstName', firstName)
+
+  renderCount++;
   return (
-    <div>App</div>
+    <form>
+      <button type='button'>{renderCount}</button>
+      <br />
+      <br />
+      <input type="text" {...register('firstName', {
+        required: true,
+        minLength: 3
+      })} />
+      <br />
+      <input type="text" {...register('lastName', {
+        required: true,
+        minLength: 3
+      })} />
+    </form>
   )
 }
 
