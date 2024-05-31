756a632819894e885690487cde2b8841cff37644
dibya.borah <dibyajyotiborah469@gmail.com>
2024-05-31 20:27:46 +0530

    handleSubmit is almost complete but not fully

--- src/App.tsx
+++ src/App.tsx
@@ -5,58 +5,33 @@ type FormValues = {
   firstName: string;
   customError: string;
 }
+
+const sleep = (ms: number) => {// This function returns a promise but this is in the function defination
+  // We are not calling the function which returns a promise . Just defining it !!!
+  return new Promise ((resolve) =>  setTimeout(resolve, ms));
+}
+
 const App = () => {
-  const { register, handleSubmit, formState: { errors }, setError } = useForm<FormValues>({
-    defaultValues: {
-      firstName: ''
-    },
-    criteriaMode: 'all',
-  });
-  // const onSubmit = (data: FormValues) => {
-  //   console.log('data', data);
-  // };
+  const { register, handleSubmit } = useForm<FormValues>();
+  const onSubmit = async (data: FormValues) => {
+    await sleep(1000);// In 1sec the result gets resolved
+    console.log('data', data);
+  };
+  const onError = () => {
+    console.log('Wrong!!!');
+  }
   renderCount++;
   return (
     <div>
-      {/* <form onSubmit={handleSubmit(onSubmit)}> */}
-      <form onSubmit={handleSubmit((data) => console.log('submitted data', data))}>
+      <form onSubmit={handleSubmit(onSubmit, onError)}>
         <button type='button'>{renderCount}</button>
         <br />
-        {/* The source of truth for submission block regarding error will be the validation object passed in register function */}
-        {/* Or the validation schema passed in hookForm resolvers */}
-        {/* The source of submission validation won't be the manuall error set using setError API */}
-
-        {/* When pressed submit the handleSubmit of react-hook-form actually ignores the error set using setErrors and will  look for the schema or register API errors regarding submission errors*/}
-        {/* <input type="text" {...register('firstName')} */}
+        {/* <input type="text" {...register('firstName', { disabled: true })} */}
         <input type="text" {...register('firstName', { required: true })}
           placeholder="First Name"
         />
-        {errors?.firstName && <div style={{ color: 'red' }}>{errors?.firstName?.types?.test}</div>}
-        {errors?.firstName && <div style={{ color: 'red' }}>{errors?.firstName?.types?.test1}</div>}
         <br />
         <br />
-        <button type="button" onClick={() => 
-          // setError('firstName', {
-          //   type: 'custom',
-          //   message: 'Something went wrong!!!'
-          // })
-          // setError('firstName', {
-          //   types: {
-          //     test: 'test is wrong',
-          //     test1: 'test1 is wrong'
-          //   }
-          // })
-          // setError('customError', {
-          //   type: 'server side',
-          //   message: 'server response false'
-          // })
-          setError('firstName', 
-          {
-            type: 'server side',
-            message: 'server response false'
-          },
-        {shouldFocus: true})
-        }>setErrors</button>
         <input type="submit" />
       </form>
     </div>
@@ -64,9 +39,3 @@ const App = () => {
 }
 
 export default App;
-
-// setValue api set one a single field in form
-// Whereas when you want to bulk update the values we use resetValues
-
-// setValues we doesnot wipe-out/ remove the field completely
-// reset api wipeout or reset the fields completely
