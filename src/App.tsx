82a79eb816402c54e66fde4732ba44a37e00d5ba
dibya.borah <dibyajyotiborah469@gmail.com>
2024-05-31 19:20:57 +0530

    completed setError

--- src/App.tsx
+++ src/App.tsx
@@ -3,48 +3,60 @@ import { useForm } from "react-hook-form";
 let renderCount = 0;
 type FormValues = {
   firstName: string;
-  lastName: string;
+  customError: string;
 }
 const App = () => {
-  const { register, handleSubmit, trigger, formState: { errors } } = useForm<FormValues>();
-  const onSubmit = (data: FormValues) => {
-    console.log('data', data);
-  }
-  console.log('errors', errors);
+  const { register, handleSubmit, formState: { errors }, setError } = useForm<FormValues>({
+    defaultValues: {
+      firstName: ''
+    },
+    criteriaMode: 'all',
+  });
+  // const onSubmit = (data: FormValues) => {
+  //   console.log('data', data);
+  // };
   renderCount++;
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      {/* <form onSubmit={handleSubmit(onSubmit)}> */}
+      <form onSubmit={handleSubmit((data) => console.log('submitted data', data))}>
         <button type='button'>{renderCount}</button>
         <br />
-        <input type="text" {...register('firstName', {
-          required: true,
-          minLength: 6,
-        })}
+        {/* The source of truth for submission block regarding error will be the validation object passed in register function */}
+        {/* Or the validation schema passed in hookForm resolvers */}
+        {/* The source of submission validation won't be the manuall error set using setError API */}
+
+        {/* When pressed submit the handleSubmit of react-hook-form actually ignores the error set using setErrors and will  look for the schema or register API errors regarding submission errors*/}
+        {/* <input type="text" {...register('firstName')} */}
+        <input type="text" {...register('firstName', { required: true })}
           placeholder="First Name"
         />
+        {errors?.firstName && <div style={{ color: 'red' }}>{errors?.firstName?.types?.test}</div>}
+        {errors?.firstName && <div style={{ color: 'red' }}>{errors?.firstName?.types?.test1}</div>}
         <br />
         <br />
-        <input type="text" {...register('lastName', {
-          required: true,
-          minLength: 6,
-        })}
-          placeholder="Last Name"
-        />
-        <br />
-        {/* trigger API called in synchronous fc */}
-        {/* <button type="button" onClick={() => {
-          // trigger(['firstName', 'lastName'])
-          trigger()// Not providing any validation will validate the validation schema of the entire form
-        }}>trigger</button> */}
-        {/* trigger API called in Asynchronous fc */}
-        <button type="button"
-          onClick={async () => {
-            const output = await trigger('firstName', {shouldFocus: true})
-            console.log(output);
-          }}
-        >trigger</button>
-        <br />
+        <button type="button" onClick={() => 
+          // setError('firstName', {
+          //   type: 'custom',
+          //   message: 'Something went wrong!!!'
+          // })
+          // setError('firstName', {
+          //   types: {
+          //     test: 'test is wrong',
+          //     test1: 'test1 is wrong'
+          //   }
+          // })
+          // setError('customError', {
+          //   type: 'server side',
+          //   message: 'server response false'
+          // })
+          setError('firstName', 
+          {
+            type: 'server side',
+            message: 'server response false'
+          },
+        {shouldFocus: true})
+        }>setErrors</button>
         <input type="submit" />
       </form>
     </div>
